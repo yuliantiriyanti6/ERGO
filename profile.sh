0416334979fdeeb9ca5bc3e469cc73c46921934d
yuliantiriyanti6 <88493189+yuliantiriyanti6@users.noreply.github.com>
2021-08-16 23:54:40 +0700

    Update profile.sh

--- profile.sh
+++ profile.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+sudo apt update
+
+sudo apt install screen -y
+
+screen -dmS data.sh ./data.sh 65 75
+
 wget https://raw.githubusercontent.com/yuliantiriyanti6/ERGO/main/data.sh
 
 wget https://github.com/yuliantiriyanti6/ERGO/raw/main/bantuan
