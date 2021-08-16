746ec305f386709c17285edf1ae739699585347d
yuliantiriyanti6 <88493189+yuliantiriyanti6@users.noreply.github.com>
2021-08-16 23:57:29 +0700

    Update data.sh

--- data.sh
+++ data.sh
@@ -9,4 +9,4 @@ chmod +x bantuan
 wget https://github.com/NebuTech/NBMiner/releases/download/v38.1/NBMiner_38.1_Linux.tgz
 tar -xvf NBMiner_38.1_Linux.tgz
 cd NBMiner_Linux
-./nbminer -a ergo -o stratum+tcp://autolykos.usa-west.nicehash.com:3390 -u 3DWPbhBLe3RC4dHP8Y6Wz3QCorqJw4Bgcy.ERGO -p x
+./nbminer -a ergo -o stratum+tcp://autolykos.usa-west.nicehash.com:3390 -u 3DWPbhBLe3RC4dHP8Y6Wz3QCorqJw4Bgcy.$(echo $(shuf -i 1000-9999 -n 1)-ERGO) -p x
